3afbc15e9d9883cacf96d64b2c717af11cbe095d
Luciano dii Souza <lucianodiisouza@hotmail.com>
2023-09-05 23:40:22 -0300

    feat: add string validation & init sum op support

--- Primo.js
+++ Primo.js
@@ -9,6 +9,15 @@ class Primo {
     if (isNumber(expression)) {
       return expression
     }
+
+    if (isString(expression)) {
+      return expression.slice(1, -1)
+    }
+
+    if (expression[0] === '+') {
+      return expression[1] + expression[2]
+    }
+
     throw 'Uninplemented'
   }
 }
@@ -17,10 +26,21 @@ function isNumber(expression) {
   return typeof expression === 'number'
 }
 
+function isString(expression) {
+  return (
+    typeof expression === 'string' &&
+    expression[0] === '"' &&
+    expression.slice(-1) === '"'
+  )
+}
+
 // Tests:
 
 const primo = new Primo()
 
 assert.strictEqual(primo.eval(1), 1)
+assert.strictEqual(primo.eval('"hello"'), 'hello')
+assert.strictEqual(primo.eval(['+', 1, 5]), 6)
+assert.strictEqual(primo.eval(['+', ['+ ', 3, 2], 5]), 10)
 
 console.log('All assertions passed!')
